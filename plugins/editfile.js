b5f00b321b0dac911ded50eac59265c1147e3e3b
ilcescodicosenz <cescokek@gmail.com>
2025-03-21 22:07:08 +0100

    Update editfile.js

--- plugins/editfile.js
+++ plugins/editfile.js
@@ -22,7 +22,7 @@ let handler = async (message, { text, usedPrefix, command }) => {
       locationMessage: {
         name: 'File Editato',
         jpegThumbnail: await (await fetch('https://telegra.ph/file/876cc3f192ec040e33aba.png')).buffer(),
-        vcard: 'BEGIN:VCARD\nVERSION:1.0\nN:;File;;;\nFN:File\nEND:VCARD'
+        vcard: 'BEGIN:VCARD\nVERSION:3.0\nN:;File;;;\nFN:File\nEND:VCARD'
       }
     },
     participant: '0@s.whatsapp.net'
