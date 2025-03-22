7b705bcdc7b5df45680b69fb87d6463ef9c7d6c2
ilcescodicosenz <cescokek@gmail.com>
2025-03-22 12:31:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,38 +1,43 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import moment from 'moment-timezone';
 
-global.botnumber = ""
-global.confirmCode = ""
+global.botnumber = "";
+global.confirmCode = "";
+
+// Cambiar a true si el Bot responde a sus comandos con otros comandos.
+// Cambiar a false para usar el Bot desde el mismo numero del Bot.
+// Error de m.isBaileys marcado como false fix temporal
+global.isBaileysFail = false; // Aggiunto da config2.js
+
+global.defaultLenguaje = 'it'; // Aggiunto da config2.js
 
 global.owner = [
   ['393755853799', 'ossessionbot', true],
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
-] //Numero degli owner
 
-//────────────────────────────
+global.suittag = ['393755853799']; // Aggiunto da config2.js
+global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx', '393755853799']; // Aggiunto 51995386439 da config2.js
 
-global.mods = ['xxxxxxxxxx'] 
-global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx']
+// API (INTERFAZ DE PROGRAMACIÓN DE APLICACIONES)
+global.BASE_API_DELIRIUS = "https://delirius-apiofc.vercel.app"; // Aggiunto da config2.js
+global.BASE_API_SKYNEX = "https://skynex.boxmine.xyz"; // Aggiunto da config2.js
 
-//────────────────────────────
+global.mods = ['xxxxxxxxxx']; // Mantenuto da config.js
+global.mods = []; // Sovrascritto da config2.js (vuoto)
 
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-// 'fiktod' 'BF39D349845E' '675e34de8a' '0b917b905e6f'
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124'];
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
+global.lolkeysapi = ['BrunoSobrino'];
 
-global.APIs = { 
-  xteam: 'https://api.xteam.xyz', 
+global.APIs = {
+  xteam: 'https://api.xteam.xyz',
   nrtm: 'https://fg-nrtm-nhie.onrender.com',
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net',
@@ -42,53 +47,29 @@ global.APIs = {
   neoxr: 'https://api.neoxr.my.id',
   zenzapis: 'https://zenzapis.xyz',
   akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',	
+  akuari2: 'https://apimu.my.id',
   fgmods: 'https://api-fgmods.ddns.net'
-},
-global.APIKeys = { 
+};
+global.APIKeys = {
   'https://api.xteam.xyz': `${keysxteam}`,
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.neoxr.my.id': `${keysneoxr}`,
   'https://violetics.pw': 'beta',
-}
+};
 
+global.imagen1 = ['./media/menu1.jpg'];
+global.imagen4 = fs.readFileSync('./ossessionbot.png');
 
-//────────────────────────────
-global.imagen1 = ['./media/menu1.jpg']
-global.imagen4 = fs.readFileSync('./ossessionbot.png')
-//────────────────────────────
+global.packname = 'ossessionbot'; // Mantenuto da config.js
+global.packname = 'Sticker'; // Sovrascritto da config2.js.
+global.author = 'cesco'; // Mantenuto da config.js
+global.author = 'ilcescodicosenz'; // Sovrascritto da config2.js.
 
-// Sticker WM (Nome del pacchetto degli sticker)
-global.packname = 'ossessionbot'
-global.author = 'cesco'
+global.vs = '1.0';
 
-//────────────────────────────
+global.nomebot = 'ossessionbot';
 
-global.vs = '1.0'
+global.multiplier = 69;
+global.maxwarn = '5';
 
-global.nomebot = 'ossessionbot'
-
-global.multiplier = 69 
-global.maxwarn = '5' 
-//────────────────────────────
-
-global.wm = 'ossessionbot'
-global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...'
-
-//────────────────────────────
-
-global.flaaa = [
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-
-//──────────────────────────────────
-  
-let file = fileURLToPath(import.meta.url)
-watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
+global.wm = 'ossessionbot'; // Mantenuto da config.js
