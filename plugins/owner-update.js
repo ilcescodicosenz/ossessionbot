d9fa1db7f1806bdf66b2f815b39eabb2bf285e3d
ilcescodicosenz <cescokek@gmail.com>
2025-03-31 21:53:54 +0200

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,18 +1,14 @@
 import { execSync } from 'child_process'
-
 let handler = async (m, { conn, text }) => {
-    m.react(done)
-    
-    if (conn.user.jid == conn.user.jid) {
-        let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-        //require('fs').readdirSync('plugins').map(v => global.reload('', v))
-        conn.reply(m.chat, stdout.toString(), m)
-    }
-}
-
+await m.react('🕓')
+if (conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+await conn.reply(m.chat, stdout.toString(), m)
+await m.react('✅')
+}}
 handler.help = ['aggiornabot']
 handler.tags = ['owner']
-handler.command = ['update', 'aggiorna', 'fix', 'correggi']
+handler.command = ['update', 'aggiornamento', 'aggiornabot'] 
 handler.rowner = true
 
 export default handler
