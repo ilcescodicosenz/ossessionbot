93054eeb6e1a84b46f7ca04c0a1342c2cd916d47
ilcescodicosenz <cescokek@gmail.com>
2025-03-31 20:35:42 +0200

    Update deleteplugin.js

--- plugins/deleteplugin.js
+++ plugins/deleteplugin.js
@@ -11,15 +11,15 @@ import {
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
 
-  let ar = Object.keys(plugins)
-  let ar1 = ar.map(v => v.replace('.js', ''))
-  if (!text) throw `📌 *_Esempio di utilizzo:_*\n*#deleteplugin nome-plugin*`
-  if (!ar1.includes(args[0])) 
-    return m.reply(`*🗃️ NON TROVATO!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    let ar = Object.keys(plugins)
+    let ar1 = ar.map(v => v.replace('.js', ''))
+    if (!text) throw `📌 *_Esempio di utilizzo:_*\n*#deleteplugin nome-plugin*`
+    if (!ar1.includes(args[0])) 
+        return m.reply(`*🗃️ NON TROVATO!*\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map(v => ' ' + v).join`\n`}`)
 
-  const file = join(__dirname, '../plugins/' + args[0] + '.js')
-  unlinkSync(file)
-  conn.reply(m.chat, `⚠️ *_Il plugin "plugins/${args[0]}.js" è stato eliminato con successo._*`, m)
+    const file = join(__dirname, '../plugins/' + args[0] + '.js')
+    unlinkSync(file)
+    conn.reply(m.chat, `⚠️ *_Il plugin "plugins/${args[0]}.js" è stato eliminato con successo._*`, m)
 
 }
 
