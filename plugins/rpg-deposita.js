1cdb3560b4fd02ef591d0c65454c84fe6df405c0
ilcescodicosenz <cescokek@gmail.com>
2025-03-31 23:18:47 +0200

    Update rpg-deposita.js

--- plugins/rpg-deposita.js
+++ plugins/rpg-deposita.js
@@ -1,15 +1,15 @@
 let handler = async (m, { args }) => {
     let user = global.db.data.users[m.sender]
     
-    if (!args[0]) return m.reply('🚩 Inserisci la quantità di *💶 Unitycoins* che vuoi depositare.')
-    if ((args[0]) < 1) return m.reply('🚩 Inserisci una quantità valida di *💶 Unitycoins*.')
+    if (!args[0]) return m.reply('🚩 Inserisci la quantità di *💶 Ossessioncoins* che vuoi depositare.')
+    if ((args[0]) < 1) return m.reply('🚩 Inserisci una quantità valida di *💶 Ossessioncoins*.')
     
     // Deposito di tutto il saldo
     if (args[0] === 'all') {
        let quantita = parseInt(user.limit)
        user.limit -= quantita
        user.bank += quantita
-       await m.reply(`Hai depositato *${quantita} 💶 Unitycoins* nella Banca.`)
+       await m.reply(`Hai depositato *${quantita} 💶 Ossessioncoins* nella Banca.`)
        return
     }
     
@@ -17,13 +17,13 @@ let handler = async (m, { args }) => {
     
     let quantita = parseInt(args[0])
     
-    if (!user.limit) return m.reply('Non hai *💶 Unitycoins* nel tuo portafoglio.')
-    if (user.limit < quantita) return m.reply(`Hai solo *${user.limit} 💶 Unitycoins* nel portafoglio.`)
+    if (!user.limit) return m.reply('Non hai *💶 Ossessioncoins* nel tuo portafoglio.')
+    if (user.limit < quantita) return m.reply(`Hai solo *${user.limit} 💶 Ossessioncoins* nel portafoglio.`)
     
     // Esegui il deposito
     user.limit -= quantita
     user.bank += quantita
-    await m.reply(`Hai depositato *${quantita} 💶 Unitycoins* nella Banca.`)
+    await m.reply(`Hai depositato *${quantita} 💶 Ossessioncoins* nella Banca.`)
  }
  
  handler.help = ['deposita']
