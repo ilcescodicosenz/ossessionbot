ec85d217db9d1e6851acb1b04936883c09604495
ilcescodicosenz <cescokek@gmail.com>
2025-04-01 14:21:22 +0200

    Update gp-id.js

--- plugins/gp-id.js
+++ plugins/gp-id.js
@@ -1,11 +1,10 @@
-var handler = async (m, { conn, groupMetadata }) => {
-    let groupId = await groupMetadata.id;
-    let channelId = groupMetadata.announce ? await groupMetadata.id.replace("-", "-announce") : "Nessun canale associato";
+var handler = async (m, { conn, groupMetadata, isGroup }) => {
+    let groupId = isGroup ? await groupMetadata.id : "Non applicabile";
+    let channelId = !isGroup ? m.chat : (groupMetadata.announce ? await groupMetadata.id.replace("-", "-announce") : "Nessun canale associato");
 
     conn.reply(m.chat, `ⓘ 𝐋' 𝐢𝐝 𝐝𝐢 𝐪𝐮𝐞𝐬𝐭𝐨 𝐠𝐫𝐮𝐩𝐩𝐨 è: ${groupId}\nⓘ 𝐋' 𝐢𝐝 𝐝𝐞𝐥 𝐜𝐚𝐧𝐚𝐥𝐞 è: ${channelId}`, m);
 }
 
 handler.command = /^(id|gpid|gcid)$/i;
-handler.group = true;
 
 export default handler;
