cc8f9cdabd15a564e2d213945127d4c874c26835
ilcescodicosenz <cescokek@gmail.com>
2025-04-02 22:14:25 +0200

    antilinkhardcanale.js

--- plugins/antilinkhardcanale.js
+++ plugins/antilinkhardcanale.js
@@ -23,7 +23,7 @@ export async function before(msg, { isAdmin, isBotAdmin }) {
         },
         'message': {
           'locationMessage': {
-            'name': "𝐀𝐧𝐭𝐢 - 𝐂𝐚𝐧𝐚𝐥𝐞",
+            'name': "𝐀𝐧𝐭𝐢 - linkhard",
             'jpegThumbnail': await (await fetch("https://telegra.ph/file/a3b727e38149464863380.png")).buffer(),
             'vcard': "BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD"
           }
