16dabb4145df2dc0baea6c6fd7ede08dd6ed891c
ilcescodicosenz <cescokek@gmail.com>
2025-04-02 22:07:42 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -79,6 +79,7 @@ export async function handler(chatUpdate) {
                 if (!('antielimina' in chat)) chat.antielimina = false
                 if (!('antiLink' in chat)) chat.antiLink = true
                 if (!('antiinsta' in chat)) chat.antiinsta = false
+                if (!('anticanale' in chat)) chat.anticanale = true
                 if (!('antitiktok' in chat)) chat.antitiktok = false
                 if (!('antiLink2' in chat)) chat.antiLink2 = false
                 if (!('antiviewonce' in chat)) chat.antiviewonce = false
@@ -113,6 +114,7 @@ export async function handler(chatUpdate) {
                     antiLink2: false,
                     antilinkbase: false,
                     antitiktokbase: false,
+                    anticanale: true,
                     antiinsta: true,
                     antitiktok: true,
                     antiviewonce: false,
