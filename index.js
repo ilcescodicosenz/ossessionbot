4291fe3c294c4e8beffd3c30a0b8100f35a1f65c
ilcescodicosenz <cescokek@gmail.com>
2025-05-25 12:15:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ say('OSSESSION\nBot\n1.0', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta']})
-    say(`developed by cesco`, {
+    say(`developed by chatunity`, {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta']})
